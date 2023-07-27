2f2db3081c39ce6e71907fe9af7c2f881af41d05
INE Codes <92663561+INE-KRISH@users.noreply.github.com>
2023-07-27 11:26:05 +0530

    Add files via upload

---
+++ user input and giving output of same.c
@@ -0,0 +1,16 @@
+int main(){
+    char name[30];
+    printf("Enter your name: \n");
+    scanf("%s", &name);
+    printf("your name: %s\n", name);
+    
+    int num1;
+    printf("Enter your number: \n");
+    scanf("%d", &num1);
+    printf("your number: %d\n", num1);
+    return 0;
+
+    return 0;
+}
+
+
